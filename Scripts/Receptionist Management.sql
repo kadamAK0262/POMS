db4545bab9416b09cfd5a07c630ee0697a7b58c2
Akshay Kadam <89634956+kadamAK0262@users.noreply.github.com>
2023-01-20 12:35:20 +0530

    Update Receptionist Management.sql

--- Scripts/Receptionist Management.sql
+++ Scripts/Receptionist Management.sql
@@ -8,7 +8,9 @@ CREATE TABLE IF NOT EXISTS Receptionist
     Gender VARCHAR(20),
     Shift_time VARCHAR(50),
     Contact_no BIGINT,
-    Address VARCHAR(50));
+    Address VARCHAR(50)
+	admin_id int not null,
+    foreign key(admin_id) references admin_reg(admin_id));
     
 
 INSERT INTO
@@ -17,12 +19,12 @@ INSERT INTO
  Designation,
  Gender, 
  Shift_time, 
- Contact_no, Address)
+ Contact_no, Address,admin_id)
  
  VALUES
  (1, "Akshay Kadam",
  "Counter_receptionist", 
- "Male",  "MORNING 09:00AM-06:00PM", 9122546582, "kopargaon");
+ "Male",  "MORNING 09:00AM-06:00PM", 9122546582, "kopargaon",1);
  select *from receptionist;
  
 
